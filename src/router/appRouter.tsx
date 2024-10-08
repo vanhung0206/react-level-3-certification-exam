ac604974ac797abaad5d60884b43d0f8c6dfe332
Nguyen Hung <hungnguyenvan@itrvn.com>
2024-10-08 07:44:54 +0700

    Change to hashrouter

--- src/router/appRouter.tsx
+++ src/router/appRouter.tsx
@@ -1,41 +1,36 @@
-import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
+import { createHashRouter } from "react-router-dom";
 import ErrorBoundary from "../components/ErrorBoundary";
+import MainLayout from "../layouts/MainLayout";
 import Exercise1 from "../pages/Exercise1";
 import Exercise2 from "../pages/Exercise2";
 import Exercise3 from "../pages/Exercise3";
 import Main from "../pages/Main";
 
-const appRouter = createBrowserRouter(
-  [
-    {
-      element: <MainLayout />,
-      id: "main",
-      path: "/",
-      errorElement: <ErrorBoundary />,
-      children: [
-        {
-          index: true,
-          element: <Main />,
-        },
-        {
-          path: "exercise-1",
-          element: <Exercise1 />,
-        },
-        {
-          path: "exercise-2",
-          element: <Exercise2 />,
-        },
-        {
-          path: "exercise-3",
-          element: <Exercise3 />,
-        },
-      ],
-    },
-  ],
+const appRouter = createHashRouter([
   {
-    basename: "/react-level-3-certification-exam",
-  }
-);
+    element: <MainLayout />,
+    id: "main",
+    path: "/",
+    errorElement: <ErrorBoundary />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: "exercise-1",
+        element: <Exercise1 />,
+      },
+      {
+        path: "exercise-2",
+        element: <Exercise2 />,
+      },
+      {
+        path: "exercise-3",
+        element: <Exercise3 />,
+      },
+    ],
+  },
+]);
 
 export default appRouter;
